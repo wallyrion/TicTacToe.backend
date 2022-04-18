417f21304926775eaab3150bdfa501af7d965418
Oleksii Korniienko <alexey.kornienko@smartodds.co.uk>
2022-04-18 16:30:23 +0300

    remove unused code

--- TicTacToe.DAL/TicTacToeContext.cs
+++ TicTacToe.DAL/TicTacToeContext.cs
@@ -10,6 +10,7 @@ public sealed class TicTacToeContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        // todo move to the config
         optionsBuilder.UseCosmos(
             "https://tictactoe-wallyrion.documents.azure.com:443/",
             "BQOkP3R4zgJbdHwAUxr2EjrTRNuoQovIC8Fi4rRhpFS1KtMi6tDGmPKTrs47e5vKsanHZQLykInYK8bPvq6Yyw==",
@@ -27,8 +28,6 @@ public sealed class TicTacToeContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        #region PartitionKey
-
         modelBuilder.Entity<User>(entity =>
         {
             entity.ToContainer("Users")
@@ -36,9 +35,6 @@ public sealed class TicTacToeContext : DbContext
                 .HasKey(x => x.Id)
                 ;
             entity.OwnsMany(x => x.RefreshTokens);
-            /*entity.OwnsMany(x => x.RefreshTokens);*/
-            /*entity.HasMany(x => x.RefreshTokens)
-                .WithOne(x => x.User);*/
 
         });
             
@@ -52,65 +48,5 @@ public sealed class TicTacToeContext : DbContext
         modelBuilder.Entity<Game>()
             .OwnsOne(x => x.Outcome);
 
-        //modelBuilder.Entity<RefreshToken>(entity =>
-        //{
-        //    entity.ToContainer("RefreshTokens")
-        //        .HasPartitionKey(x => x.UserId)
-        //        .HasKey(x => x.Id);
-
-        //    entity.OwnsOne(x => x.User);
-        //    //entity.OwnsOne(x => x.User);
-        //    /*entity.HasOne(d => d.User)
-        //        .WithMany(x => x.RefreshTokens)
-        //        .HasForeignKey(x => x.UserId);*/
-        //});
-
-        #endregion
-
-
-        //#region DefaultContainer
-        //modelBuilder.HasDefaultContainer("Store");
-        //#endregion
-
-        //#region Container
-        //modelBuilder.Entity<Users>()
-        //    .ToContainer("Orders");
-        //#endregion
-
-        //#region NoDiscriminator
-        //modelBuilder.Entity<Order>()
-        //    .HasNoDiscriminator();
-        //#endregion
-
-        //#region PartitionKey
-        //modelBuilder.Entity<Order>()
-        //    .HasPartitionKey(o => o.PartitionKey);
-        //#endregion
-
-        //#region ETag
-        //modelBuilder.Entity<Order>()
-        //    .UseETagConcurrency();
-        //#endregion
-
-        //#region PropertyNames
-        //modelBuilder.Entity<Order>().OwnsOne(
-        //    o => o.ShippingAddress,
-        //    sa =>
-        //    {
-        //        sa.ToJsonProperty("Address");
-        //        sa.Property(p => p.Street).ToJsonProperty("ShipsToStreet");
-        //        sa.Property(p => p.City).ToJsonProperty("ShipsToCity");
-        //    });
-        //#endregion
-
-        //#region OwnsMany
-        //modelBuilder.Entity<Distributor>().OwnsMany(p => p.ShippingCenters);
-        //#endregion
-
-        //#region ETagProperty
-        //modelBuilder.Entity<Distributor>()
-        //    .Property(d => d.ETag)
-        //    .IsETagConcurrency();
-        //#endregion
     }
 }
